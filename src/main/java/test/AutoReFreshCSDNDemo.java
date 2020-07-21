59d7d65380693c238431fcf41f084d55dc68f32b
1015770492@qq.com <1015770492@qq.com>
2020-07-21 16:08:01 +0800

    上传jar包

--- src/main/java/test/AutoReFreshCSDNDemo.java
+++ src/main/java/test/AutoReFreshCSDNDemo.java
@@ -10,7 +10,7 @@ public class AutoReFreshCSDNDemo {
 
         CSDNUtils csdnUtils = new CSDNUtils();
 //        csdnUtils.autoRefresh(args[0],Integer.parseInt(args[1]));//csdn设置了时间大概1分钟
-//        csdnUtils.autoRefresh("https://blog.csdn.net/qq_44772414",50);//csdn设置了增加访问量的时间，时间大概1分钟
+        csdnUtils.autoRefresh("https://blog.csdn.net/qq_44772414",50);//csdn设置了增加访问量的时间，时间大概1分钟
         csdnUtils.autoRefresh("https://blog.csdn.net/qq_41813208/",50);//csdn设置了时间大概1分钟
     }
 
