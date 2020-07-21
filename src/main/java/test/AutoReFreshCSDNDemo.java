fb3aa00d7a9f513971c26113c10d66eff71f5625
1015770492@qq.com <1015770492@qq.com>
2020-07-21 15:31:21 +0800

    采用java8重新编写代码，加入定时任务的线程池。 lambda表达式，并行流，定时任务。

--- src/main/java/test/AutoReFreshCSDNDemo.java
+++ src/main/java/test/AutoReFreshCSDNDemo.java
@@ -1,16 +1,17 @@
 package test;
 
-import org.springframework.web.client.RestTemplate;
 import util.CSDNUtils;
+
+
 
 public class AutoReFreshCSDNDemo {
 
     public static void main(String[] args) {
-        RestTemplate restTemplate=new RestTemplate();
+
         CSDNUtils csdnUtils = new CSDNUtils();
 //        csdnUtils.autoRefresh(args[0],Integer.parseInt(args[1]));//csdn设置了时间大概1分钟
-        csdnUtils.autoRefresh("https://blog.csdn.net/qq_44772414",50);//csdn设置了时间大概1分钟
-//        csdnUtils.autoRefresh("https://blog.csdn.net/qq_41813208/",50);//csdn设置了时间大概1分钟
+//        csdnUtils.autoRefresh("https://blog.csdn.net/qq_44772414",50);//csdn设置了增加访问量的时间，时间大概1分钟
+        csdnUtils.autoRefresh("https://blog.csdn.net/qq_41813208/",50);//csdn设置了时间大概1分钟
     }
 
 }
--- src/main/java/util/CSDNUtils.java
+++ src/main/java/util/CSDNUtils.java
@@ -8,34 +8,18 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class CSDNUtils {
     //    发送http请求的工具类
     private RestTemplate restTemplate = new RestTemplate();
-    private List<String> myAllArticleURL;
-    ExecutorService cachedThreadPool = Executors.newCachedThreadPool();
-    private AtomicInteger day=new AtomicInteger(new Date().getDay());
+    private Set<String> myAllCategoryURL = new CopyOnWriteArraySet<>();//去重
+    private Set<String> myAllArticleURL = new CopyOnWriteArraySet<>();//去重，并发访问
+    ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(20);
+    private AtomicLong count = new AtomicLong(0);
 
-
-    /**
-     * 只有调用了getAllArticleUrl才会有
-     *
-     * @return
-     */
-    public List<String> getMyAllArticleURL() {
-        if (myAllArticleURL == null) {
-            getAllArticleUrl("https://blog.csdn.net/qq_41813208/");
-        }
-        return myAllArticleURL;
-    }
 
     /**
      * 获取博主所有的分类专栏访问地址
@@ -43,7 +27,8 @@ public class CSDNUtils {
      *
      * @param url
      */
-    public List<String> getAllBlogCategoryURL_By_URL(String url) {
+    public void addAllBlogCategoryURL_By_URL(String url) {
+        myAllCategoryURL.clear();//清空之前存的所有分类url集合
         ResponseEntity<String> htmlString = restTemplate.getForEntity(url, String.class);
         String html = htmlString.toString();//将获取的网页转换成字符串
         //获取html元素
@@ -53,17 +38,17 @@ public class CSDNUtils {
         //  获取id=asideCategory的标签下的ul标签
         Elements ultag = asideCategory.getElementsByTag("ul");
         // 获取id=asideCategory的标签下的ul标签的a标签===>这个标签的href存放的就是分类专栏地址
-        Elements a = ultag.get(0).getElementsByTag("a");
-        List<String> categoryURL = new ArrayList<>();
+        Elements a_s = ultag.get(0).getElementsByTag("a");
         //遍历a标签，获取a标签中的href属性值
-        for (int i = 0; i < a.size(); i++) {
-            String href = a.get(i).attr("href");//获取href属性
+        a_s.parallelStream().forEach((a) -> {
+            String href = a.attr("href");
             if (!href.isEmpty()) {
-                categoryURL.add(i, href);//存入数组中
+                myAllCategoryURL.add(href);//并行流添加到集合中
             }
-        }
-        System.out.println("总共有" + categoryURL.size() + "个专栏");
-        return categoryURL;
+        });
+
+        String log = "总共有" + myAllCategoryURL.size() + "个专栏";
+        System.out.println(log);
     }
 
     /**
@@ -72,112 +57,96 @@ public class CSDNUtils {
      * @param url
      * @return
      */
-    public List<String> getAllArticle_By_CategoryURL(String url) {
+    public void addAllArticle_By_CategoryURL(String url) {
         ResponseEntity<String> htmlString = restTemplate.getForEntity(url, String.class);
         String html = htmlString.toString();//将获取的网页转换成字符串
         // 获取html元素
         Document doc = Jsoup.parse(html);
+        String title = doc.getElementById("column").getElementsByClass("column_title oneline").get(0).text();//获取专栏标题
+
         //1、 获取类名为column_article_list的html标签，这个标签内存放的就是文章的列表
         Elements column_article_list = doc.getElementsByAttributeValue("class", "column_article_list");
-        // 获取包含专栏文章的的li元素
+        //2、 获取专栏中所有文章的li元素
         Elements li_s = column_article_list.get(0).getElementsByTag("li");
-        List<String> allArticle = new ArrayList<>();//用来存单个专栏下所有文章的链接
-        //li标签有很多个，遍历li标签
-        for (int i = 0; i < li_s.size(); i++) {
-            //获取li中的a标签
-            Elements a = li_s.get(i).getElementsByTag("a");
-            //获取a标签的href属性
-            String href = a.attr("href");
-            //如果href元素为空则不存入数组中
-            if (!href.isEmpty()) {//不为空则存入数组，有些专栏可能没有文章，所以这里要判断一下
-                allArticle.add(href);
+        //3、并行流处理，遍历获取文章url
+        li_s.parallelStream().forEach((li) -> {
+            String href = li.getElementsByTag("a").attr("href");
+            if (!href.isEmpty()) {
+                myAllArticleURL.add(href);//存入成员变量中
             }
-        }
-        //将存有单个专栏的所有文章链接数组返回
-        return allArticle;
+        });
+
+        System.out.println(title + "专栏有" + li_s.size() + "篇文章");
     }
 
 
     /**
-     * 返回所有文章url列表
+     * 遍历所有专栏，获取所有文章
      * 会存到myAllArticleURL的成员变量上
      *
      * @param url
      * @return
      */
-    public List<String> getAllArticleUrl(String url) {
-        //调用方法获取所有分类专栏，存入allBlogCategoryURL数组中
-        List<String> allBlogCategoryURL = getAllBlogCategoryURL_By_URL(url);
-        // allArticleUrl用来存所有文章的链接
-        List<String> allArticleUrl = new ArrayList<>();
+    public Set<String> getAllArticleUrl(String url) {
+        //1、获取所有专栏url，存入成员变量myAllCategoryURL
+        addAllBlogCategoryURL_By_URL(url);
 
-        //遍历所有专栏
-        for (int i = 0; i < allBlogCategoryURL.size(); i++) {
-            //调用方法获取分类专栏下的所有文章链接
-            List<String> categoryAllArticle = getAllArticle_By_CategoryURL(allBlogCategoryURL.get(i));
-            //将分类专栏下的文章加入allArticleUrl数组中
-            allArticleUrl.addAll(categoryAllArticle);
+        //2、并行流处理获取所有专栏url
+        myAllCategoryURL.parallelStream().forEach(categoryUrl -> {
+            addAllArticle_By_CategoryURL(categoryUrl);
+        });
+
+        System.out.println("总共有" + this.myAllArticleURL.size() + "篇文章");
+        long times = count.incrementAndGet();
+        if (count.get() >= Long.MAX_VALUE - 1) {
+            count.set(0);//超过最大值则重新初始化为0
         }
-        this.myAllArticleURL = allArticleUrl;
-        return allArticleUrl;
+        System.out.println("\n\n======访问了" + times + "次=======\n\n");
+        return myAllArticleURL;//所有文章地址
     }
 
     /**
-     * 访问文章详情，模拟用户点击文章
+     * 定时的访问文章详情，模拟用户点击文章
+     * @param url
+     * @param scheduleTime
+     */
+    public void accessCSDNArticle(String url, Long scheduleTime) {
+        scheduledExecutorService.scheduleAtFixedRate(() -> {
+            ResponseEntity<String> forEntity = new RestTemplate().getForEntity(url, String.class);
+            if (forEntity.getStatusCode() == HttpStatus.OK) {
+                System.out.println(url + "访问成功");
+            } else {
+                System.out.println(url + "访问失败");
+            }
+        }, 0, scheduleTime, TimeUnit.SECONDS);
+    }
+
+    /**
+     * 重新来一遍
      *
      * @param url
      */
-    public void accessCSDNArticle(String url) {
-        cachedThreadPool.execute(new Runnable() {
-            @Override
-            public void run() {
-                ResponseEntity<String> forEntity = restTemplate.getForEntity(url, String.class);
-                if (forEntity.getStatusCode() == HttpStatus.OK) {
-                    System.out.println(url + "访问成功");
-                } else {
-                    System.out.println(url + "访问失败");
-                }
-            }
-        });
-
-
+    public void updateUrl(String url) {
+        this.myAllCategoryURL.clear();//清空所有专栏地址
+        this.myAllArticleURL.clear();//清空之前的文章
+        getAllArticleUrl(url);//重新获取所有文章链接到myAllArticleURL中
     }
 
     /**
      * 前面一次性获取来所有博客链接，并保存了下来。这样就不需要再次发送获取所有文章的请求
      *
-     * @param url       传入博主自己的url主页，例如：https://blog.csdn.net/qq_41813208/
-     * @param sleepTime 休眠时间，单位秒
+     * @param url          传入博主自己的url主页，例如：https://blog.csdn.net/qq_41813208/
+     * @param scheduleTime 休眠时间，单位秒
      */
-    public void autoRefresh(String url, long sleepTime) {
+    public void autoRefresh(String url, long scheduleTime) {
         getAllArticleUrl(url);//获取所有文章链接，文章链接会存在成员变量myAllArticleURL中
-        while (true) {
-            Date date=new Date();
-            //每天定时的更新所有文章内容
-            if (this.day.get() < date.getDay()){
-                this.day.set(date.getDay());
-                this.myAllArticleURL=null;//清空之前的文章
-                getAllArticleUrl(url);//重新获取所有文章链接到myAllArticleURL中
-            }
 
-            for (int i = 0; i < myAllArticleURL.size(); i++) {
-                if (myAllArticleURL.get(i) != null) {
-                    try {
-                        TimeUnit.MILLISECONDS.sleep(100);
-                        accessCSDNArticle(myAllArticleURL.get(i));//访问文章
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-            try {
+        scheduledExecutorService.scheduleAtFixedRate(() -> {
+            updateUrl(url);//每2小时更新一次url
+        }, 0, 2, TimeUnit.HOURS);
 
-                System.out.println(date);
-                TimeUnit.SECONDS.sleep(sleepTime);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-
+        this.myAllArticleURL.parallelStream().forEach(articleUrl -> {
+            accessCSDNArticle(articleUrl, scheduleTime);//定时的访问，防止刷新太快被封ip
+        });
     }
 }
